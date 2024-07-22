f18c2cee4ffb395c41360a733f77e572496c1654
willuigi64 <144480403+willuigi64@users.noreply.github.com>
2024-07-21 23:45:43 -0400

    init

---
+++ src/main/java/ca/georgiancollege/assignment2gc1034/HelloController.java
@@ -0,0 +1,14 @@
+package ca.georgiancollege.assignment2gc1034;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module ca.georgiancollege.assignment2gc1034 {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens ca.georgiancollege.assignment2gc1034 to javafx.fxml;
+    exports ca.georgiancollege.assignment2gc1034;
+}
