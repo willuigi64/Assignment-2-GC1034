4862ce4cafa25f9a2e03ba2cfe766180c7dd5a7f
willuigi64 <144480403+willuigi64@users.noreply.github.com>
2024-08-09 20:44:30 -0400

    Search functionality does not exist to my knowledge.

--- src/main/java/ca/georgiancollege/assignment2gc1034/APITester.java
+++ src/main/java/ca/georgiancollege/assignment2gc1034/APITester.java
@@ -13,12 +13,15 @@ public class APITester {
     //tests with changing around the URI, my project will
     //no longer be about searching a list, rather it will be about
     //selecting a platform and then selecting one of the games in the
-    //list (documented is max 100 complete objects, but I've recieved 110 twice now
+    //list (documented is max 100 complete objects)
     static void searchTest(){
         APIUtility api = new APIUtility();
         String json = api.sendRequest("https://api.mobygames.com/v1/games?api_key=moby_B7XtgpO1L9MbBw8SfOmdd8pk2n2&?q=final+fantasy&type=game&format=brief");
         System.out.println(json);
     }
+    //conclusion: no search functionality. solution?
+    //user choice comes in the form of selecting a platform,
+    //or a random game.
 
     public static void main(String[] args) {
 
